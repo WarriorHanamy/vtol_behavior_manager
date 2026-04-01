fc3b412b8d751841902f42558eabe7b52a2ccdba
WarriorHanamy <632029496@qq.com>
2026-04-01 19:18:38 +0800

    add entrypoint design, vibe coding too much, we don't know every details of repos

--- deps/debug_px4/src/debug_px4/__init__.py
+++ deps/debug_px4/src/debug_px4/__init__.py
@@ -1,4 +1,5 @@
+from debug_px4.entrypoint import Entrypoint, plot, record, subscribe
 from debug_px4.subscriber import ZenohSubscriber
 from debug_px4.topics import get_topic_handler
 
-__all__ = ["ZenohSubscriber", "get_topic_handler"]
+__all__ = ["Entrypoint", "ZenohSubscriber", "get_topic_handler", "plot", "record", "subscribe"]
