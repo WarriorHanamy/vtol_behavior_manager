3a1c404e950d84c3cd6b9c2d341d082f1779b2f8
WarriorHanamy <632029496@qq.com>
2026-04-07 16:40:06 +0800

    easy to be used.

--- dockerfiles/ros2_entrypoint.sh
+++ dockerfiles/ros2_entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -e
 
+# ROS2 setup.bash references unbound variables (e.g. AMENT_TRACE_SETUP_FILES).
+# Temporarily disable nounset around sourcing to avoid "unbound variable" errors.
+set +u
 source /opt/ros/humble/setup.bash
 
 if [ -f /home/ros/ros2_ws/install/setup.bash ]; then
     source /home/ros/ros2_ws/install/setup.bash
 fi
+set -u
 
 exec "$@"
