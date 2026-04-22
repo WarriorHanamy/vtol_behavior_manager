6a1f0825c6b93dcbef7a69536395c99e70f2034f
WarriorHanamy <632029496@qq.com>
2026-04-22 15:30:13 +0800

    remove unrealted modules

--- deps/debug_px4/src/debug_px4/__init__.py
+++
@@ -1,5 +0,0 @@
-from debug_px4.entrypoint import Entrypoint, plot, record, subscribe
-from debug_px4.subscriber import ZenohSubscriber
-from debug_px4.topics import get_topic_handler
-
-__all__ = ["Entrypoint", "ZenohSubscriber", "get_topic_handler", "plot", "record", "subscribe"]
--- deps/debug_px4/src/debug_px4/viz/__init__.py
+++
@@ -1,3 +0,0 @@
-from debug_px4.viz.plot import RealTimePlot
-
-__all__ = ["RealTimePlot"]
